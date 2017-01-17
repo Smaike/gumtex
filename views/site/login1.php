c0998ff68574ad620ae87b442be7b5dd450d7ded
spurrusp <spuR05@mail.ru>
2017-01-17 23:26:18 +0300

    login reg

--- views/site/login1.php
+++ views/site/login1.php
@@ -3,17 +3,20 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\LoginForm */
 /* @var $form ActiveForm */
 ?>
-<div class="login1">
-
+<div class="site-login">
+    <h1><?= Html::encode($this->title) ?></h1>
     <?php $form = ActiveForm::begin(); ?>
 
         <?= $form->field($model, 'login') ?>
-        <?= $form->field($model, 'password') ?>
-        <?= $form->field($model, 'rememberMe') ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
     
         <div class="form-group">
             <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
--- views/site/reg.php
+++ views/site/reg.php
@@ -6,19 +6,21 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\RegForm */
 /* @var $form ActiveForm */
+$this->title = 'Registration';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="reg">
 
     <?php $form = ActiveForm::begin(); ?>
 
         <?= $form->field($model, 'login') ?>
-        <?= $form->field($model, 'password') ?>
-        <?= $form->field($model, 'password2') ?>
-        <?= $form->field($model, 'first_name') ?>
+        <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'password2')->passwordInput() ?>
         <?= $form->field($model, 'email') ?>
+        <?= $form->field($model, 'first_name') ?>
         <?= $form->field($model, 'last_name') ?>
         <?= $form->field($model, 'middle_name') ?>
-
+        Осталось еще тип и что то там еще
         <?//= $form->field($model, 'status') ?>
     
         <div class="form-group">
