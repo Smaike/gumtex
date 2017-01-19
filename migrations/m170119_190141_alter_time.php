b3f281b7ead1acd93186699efd0e64710774b177
Vlad Luschikov <v.i.luschikov@gmail.com>
2017-01-19 22:07:43 +0300

    start edits

---
+++ migrations/m170119_190141_alter_time.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170119_190141_alter_time extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170119_190141_alter_time cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
