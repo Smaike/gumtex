f0990c01e955a213e82583470ac5b1b5f39d3425
Vlad <vladislav.lushchikov@itella.com>
2017-07-03 22:41:08 +0300

    fix

--- views/event/_view_copy.php
+++ views/event/_view_copy.php
@@ -1,14 +1,14 @@
 <?php 
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
-$serv = $model->lastEvent->getServices()->one();
+$serv = ($model->lastEvent)?$model->lastEvent->getServices()->one():"";
 ?>
 <b><?=$model->last_name?> <?=$model->first_name?> <?=$model->middle_name?></b><br>
 <?=$model->age?> лет <?=$model->birthday?> тел. <?=$model->mobile?><br>
 мать: <?=$model->fio_mother?> лет <?=$model->birthday?> тел. <?=$model->p_mobile?><br>
 отец: <?=$model->fio_father?> лет <?=$model->birthday?> тел. <?=$model->p_mobile?><br>
-Дата оказания последней услуги: <?=$model->lastEvent->date?><br>
+Дата оказания последней услуги: <?=($model->lastEvent)?$model->lastEvent->date:""?><br>
 Вид услуги: <?=$serv->name?><br>
-Консультант: <?=$model->consultant->fullName?><br><br>
+Консультант: <?=($model->consultant)?$model->consultant->fullName:""?><br><br>
 <button class="btn btn-primary" data-dismiss="modal" onClick="$('input[name=\'EventCreateForm[copy_id]\'][value=\'<?=$model->id?>\']').trigger('click')">Выбрать</button>
 <button class="btn btn-danger" data-dismiss="modal">Закрыть</button>
